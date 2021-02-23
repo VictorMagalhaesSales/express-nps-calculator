aa93fd7dd8251937716647879b38735ea1229e93
Victor Magalhães <victormagalhaessales@gmail.com>
2021-02-23 11:25:06 -0300

    Instalando dependências e testando servidor

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.listen(3333, () => console.log("Servidor inicializado"));
+
+app.get('/', (request, response) => {
+    return response.json({message: "Hello world"});
+});

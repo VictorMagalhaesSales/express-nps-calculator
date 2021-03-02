070165eef2d704a9ba0de094740223155c29bd82
Victor Magalhães <victormagalhaessales@gmail.com>
2021-03-02 00:36:14 -0300

    Email image

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { UserController } from "./controllers/UserController";
 import { Router } from 'express';
 import { SurveyController } from "./controllers/SurveyController";
+import { SendMailController } from "./controllers/SendMailController";
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 const router = Router();
 
 router.post('/user', userController.create);
@@ -11,5 +13,8 @@ router.get('/user', userController.listAll);
 
 router.post('/survey', surveyController.create);
 router.get('/survey', surveyController.listAll);
+
+
+router.post("/sendMail", sendMailController.execute);
 
 export {router};
